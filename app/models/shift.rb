8cd5911ec0dd0c9b6fd9eec6c3fc7766ee4da7a0
Tyler <tyler.mclean@kellogg.northwestern.edu>
2018-06-03 02:05:16 +0000

    custom validation started for shifts
    built custom validation for work days when adding a new shift. Still need to add validation for 24 hour period and convert to a warning that chiefs can override.

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -13,6 +13,19 @@
 
 class Shift < ApplicationRecord
   
+  # # Format for making custom validations
+  validate :work_hours
+    def work_hours
+        @start = (date - 13.days).to_datetime
+        @end = date
+        @shifts = Shift.where("date > ?", @start)
+        @shifts = @shifts.where("date < ?", @end)
+        if @shifts.count > 13
+          errors.add(:work_hours, "Be aware of potential duty hour violation")
+        end
+    end
+  # # End custom validation
+  
   belongs_to :user
   belongs_to :service
 
