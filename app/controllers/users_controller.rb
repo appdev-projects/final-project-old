59fc6ff9d9ee7194ef7e3699e72bfdb3b62e86ea
Tyler <tyler.mclean@kellogg.northwestern.edu>
2018-06-03 01:00:20 +0000

    shift_counter_v2_commit
    copied from previous commit for smoother merge

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,30 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params.fetch("id_to_display"))
 
+@shifts = Shift.where(user_id: @user.id)
+    @assignments = Assignment.where(user_id: @user.id)
+    @blocks = Block.all
+    @night_count = 0
+    @cross = 0
+    @shifts.each do |count|
+
+      @assignments.each do |day|
+        if count.date >= Block.find(day.block_id).start_date
+          if count.date <= Block.find(day.block_id).end_date
+            if day.service_id == count.service_id
+            else  
+              @cross = @cross + 1
+            end
+          end
+        end
+      end
+
+
+      if count.night == true
+        @night_count = @night_count + 1
+      end
+    end
+
     render("user_templates/show.html.erb")
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20180601161757) do
 
   create_table "active_admin_comments", force: :cascade do |t|
