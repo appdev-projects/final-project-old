7a804cc5c573e3270ef13f860f2ad1f36cfe7962
Wessam Abozeid <wessam@uchicago.edu>
2018-12-04 19:54:30 +0000

    removed API key

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -8,13 +8,7 @@ class PodcastsController < ApplicationController
   def show
     @podcast = Podcast.find(params.fetch("id_to_display"))
     
-    # url=HTTParty.post("https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDCtsef6mzWK2OwyhkJgV4fB5S8vG_7i4Q")
 
-    # lat= url.parsed_response.fetch("location").fetch("lat")
-    # lng= url.parsed_response.fetch("location").fetch("lng")
-    
-    # loc=HTTParty.post("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat.to_s + "," + lng.to_s + "&key=AIzaSyDCtsef6mzWK2OwyhkJgV4fB5S8vG_7i4Q")
-    # @location= loc.parsed_response.fetch("results")[0].fetch("address_components")[5].fetch("long_name")
     
     @location= request.location.city
     
